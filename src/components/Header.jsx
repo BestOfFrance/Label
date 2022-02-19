017e24db24212dc7a97ffe897f960b084e2792f3
Lisa Cormier <lcormier5@hotmail.com>
2022-02-19 11:22:06 -0700

    add search bar to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,18 @@
-import react from 'react'
+import {useState} from 'react'
 import './Header.css'
 import SearchBar from './SearchBar'
 
 
 export default function Header(props) {
+  const [hidden, setHidden] = useState('hidden')
 
+  const showSearch = function() {
+    if (hidden === 'hidden') {
+      setHidden('show')
+    } else {
+      setHidden('hidden')
+    }
+  }
   return (
     <div className="header">
       <div onClick={props.onHome}>
@@ -18,11 +26,22 @@ export default function Header(props) {
       <div>
       
         </div>
+        {hidden === 'show' && 
         <SearchBar
+        
         searchSelected={props.searchSelected}
         updateSearch={props.updateSearch}
         searchList={props.searchList}
-        />
+      />
+        
+        }
+        
+        
+        <div>
+          <img className="search-icon" src="icons8-search-50.png" onClick={showSearch}/>
+        
+        </div>
+        
       <div className="user" onClick={props.getAccount}>
         <img src="icons8-user-64.png" className="user-icon"></img>
         <div className="myaccount">
