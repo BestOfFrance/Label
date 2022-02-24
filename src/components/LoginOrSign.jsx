59e8504b39ca0378082be6fa97b9fa1bc35104f9
Lisa Cormier <lcormier5@hotmail.com>
2022-02-24 13:29:47 -0500

    add information to login

--- src/components/LoginOrSign.jsx
+++ src/components/LoginOrSign.jsx
@@ -14,7 +14,10 @@ export default function LoginOrSign(props) {
         <div className="foodie-panel">
           <button className="foodie-button"><b>Foodie</b></button> 
           <div className="foodie-button"><button className="foodie-button" onClick={props.getRegisterFoodie}><u>Register</u></button></div>
-          <div>More information here.</div>
+          <div className="login-information"><ul>
+            <li>Access more information about shops</li>
+            <li>More features to come</li>
+            </ul></div>
         </div>
         <div className="login-button">
         <button onClick={props.login}><u>Login</u></button>
@@ -22,7 +25,12 @@ export default function LoginOrSign(props) {
         <div className="business-panel">
           <button className="business-button" ><b>Business Owner</b></button>
           <div className="business-button"><button className="business-button" onClick={props.getRegister}><u>Register</u></button></div>
-          <div>More information here.</div>
+          <div className="login-information"><ul>
+            <li>Choose between Freemium and Premium</li>
+            <li>With Premium, access and edit information about your store </li>
+            <li>Access more information about shops</li>
+            <li>More features to come</li>
+            </ul></div>
         </div>
       </div>
       
