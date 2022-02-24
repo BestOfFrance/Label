0265f81565e1a163b876b2f20ed50438ad5bc6ea
Lisa Cormier <lcormier5@hotmail.com>
2022-02-24 14:48:27 -0500

    add footer content from main website

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,16 +1,134 @@
 import './footer.css'
+import './Footer.scss'
 
 export default function Footer(props) {
   return (
-    <div className="footer">
-      <div className="footer-content">
-        <div className="footer-icon-container">
-          <img className="footer-icon" src="facebook.png"/>
-          <img className="footer-icon" src="instagram.png"/>
-          <img className="footer-icon" src="youtube.png"/>
+   
+
+ 
+<footer class="footer-container" >
+  <div class="footer-center">
+    <div class=" container"> 
+      <div class="row">
+        
+         <div class="col-md-4 col-12 ">
+            
+
+           
+        <div class="footer-customhtml">
+          <h3 class="module-title">contact us </h3>
+<h5>It is time for You to Experience True France.</h5>
+<p><i class="cs-font clever-icon-map-5"></i> Vancouver, BC |
+Canada</p>
+<p><i class="cs-font clever-icon-mail-4"></i> experience@bestoffrance.ca</p>
         </div>
-        Copyright @ 2022 Best of France LLP. All rights reserved.
+        
+
+
+
+
+
+        </div>
+        <div class="col-md-4 col-12 row">
+          
+
+
+          <div class="col-md-6">
+            <div class="footer-link">
+              
+              <h3 class="module-title">Shortcuts</h3>
+              
+              <ul class="content">
+                <li><a href="/search">Search</a></li>
+                
+                <li><a href="/pages/faq">FAQ</a></li>
+                
+                <li><a href="https://shop.bestoffrance.ca/a/track-order">Track Order</a></li>
+                
+                <li><a href="/pages/manifesto">Manifesto</a></li>
+                
+              </ul>
+            </div>
+          </div>
+
+
+
+        </div>
+        <div class="col-md-4 col-12 ">
+          
+
+
+
+          
+        <div class="footer-newsletter">
+          <div class="newsletter">
+            
+            <h3 class="module-title">
+              Newsletter
+            </h3>
+            
+            <p class="sub-title">Visit our main website to subscribe to our newsletter</p>
+            
+          </div>
+        </div>
+        
+
+
+
+        </div>
+        <div class="clearfix"></div>
+       
+
       </div>
     </div>
+  </div>
+  <div class="footer-bottom  text-center">
+    
+
+
+
+
+          <div class="footer-isocal">
+          
+          <div class="footer-icon-share">
+            
+            <a href="https://www.facebook.com/bestoffranceCanada/">
+              <i class="cs-font clever-icon-facebook"></i>
+            </a>
+            
+            
+            <a href="https://www.instagram.com/bestoffrance.ca">
+              <i class="cs-font clever-icon-instagram"></i>
+            </a>
+            
+            
+            
+            
+            <a href="https://www.youtube.com/channel/UCitywNqeMADNW6lQ_zE4gXg">
+              <i class="cs-font clever-icon-youtube-2"></i>
+            </a>
+            
+
+            
+
+            
+          </div>
+        </div>
+          
+
+          <div class="footer-copyright">
+          <address>
+            Copyright © 2021 Best of France LLP. All rights reserved
+          </address>
+        </div>
+  </div>
+</footer>
+
+        
+
+
+
+       
+
   )
 }
