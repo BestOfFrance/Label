0c6f34b699929382835bc2714eeb47a257f1f9e4
Lisa Cormier <lcormier5@hotmail.com>
2022-02-25 16:06:18 -0500

    finish styling for new login page

--- src/components/LoginOrSign.jsx
+++ src/components/LoginOrSign.jsx
@@ -17,24 +17,26 @@ export default function LoginOrSign(props) {
           
           <img className="foodie-business-image" src="person-shopping-online.png"/>
           <b>Foodie</b>
-          <div className="foodie-button"><button className="foodie-button" onClick={props.getRegisterFoodie}><u>Register</u></button></div>
+          
           <div className="login-information"><ul>
             <li>Access more information about shops</li>
             <li>More features to come</li>
             </ul></div>
+            <div className="foodie-button"><button className="foodie-button" onClick={props.getRegisterFoodie}><u>Register</u></button></div>
         </div>
         
         <div className="business-panel">
          
          <img className="foodie-business-image" src="person-studying-online-2.png"/>
          <b>Business Owner</b>
-          <div className="business-button"><button className="business-button" onClick={props.getRegister}><u>Register</u></button></div>
+          
           <div className="login-information"><ul>
             <li>Choose between Freemium and Premium</li>
             <li>With Premium, access and edit information about your store </li>
             <li>Access more information about shops</li>
             <li>More features to come</li>
             </ul></div>
+            <div className="business-button"><button className="business-button" onClick={props.getRegister}><u>Register</u></button></div>
         </div>
       </div>
       
